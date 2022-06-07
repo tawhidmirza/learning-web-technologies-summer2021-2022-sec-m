b7b9cf8a2fcb956af012c099cb98d5cbbabd2902
Tawhid <mirza019333@gmail.com>
2022-06-07 03:19:40 -0700

    Tawhid

---
+++ task-1.php
@@ -0,0 +1,12 @@
+<?php 
+
+$length=5;
+$width=6;
+$area=$length*$width;
+$paramiter=2*$area;
+echo $area;
+
+echo $paramiter;
+
+
+ ?>
---
+++ task-2.php
@@ -0,0 +1,7 @@
+<?php
+$salary;
+$salary=55500;
+$rate=7;
+$ammount=(7/100)*$salary;
+echo "<h1>Amount is </h1>".$ammount;
+?>
